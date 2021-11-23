74285569f4f5451c45c72b0e6ec9c6f2589eede5
Michael Hofweller <michael.hofweller@gmail.com>
2021-11-23 13:23:16 -0800

    useRef, useContext, Input component, ContextAPI, useImperativeHandle, React.forwardRef

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,15 @@
-import React, { useState, useEffect, useReducer } from "react";
-
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
+import Input from "../UI/Input/Input";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (prevState, action) => {
   // the action is whatever is inside the brackets of dispatchEmail(), ex., dispatchEmail({type: 'USER_INPUT', val: event.target.value})
@@ -26,6 +33,11 @@ const passwordReducer = (prevState, action) => {
 };
 
 const Login = (props) => {
+  const ctx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmailReducer] = useReducer(emailReducer, {
@@ -38,15 +50,14 @@ const Login = (props) => {
     isValid: null,
   });
 
+  // further optimize useEffect, you can destructure your objects to get only the isValid value and use that value as a dependency for your useEffect so it only re-runs when the validity has changed and not re-run a validity check every time the pw field changes even after its been validated.
 
-  // further optimize useEffect, you can destructure your objects to get only the isValid value and use that value as a dependency for your useEffect so it only re-runs when the validity has changed and not re-run a validity check every time the pw field changes even after its been validated. 
-
-  const {isValid: emailIsValid} = emailState; 
-  const {isValid: pwIsValid} = pwState; 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: pwIsValid } = pwState;
 
   useEffect(() => {
     const loginTimeout = setTimeout(() => {
-      setFormIsValid(emailIsValid && pwIsValid); 
+      setFormIsValid(emailIsValid && pwIsValid);
     }, 500);
 
     return () => {
@@ -58,9 +69,7 @@ const Login = (props) => {
     // description 'type' and payload 'val'
     dispatchEmailReducer({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(
-      event.target.value.includes("@") && pwState.isValid
-    );
+    setFormIsValid(event.target.value.includes("@") && pwState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
@@ -71,54 +80,54 @@ const Login = (props) => {
 
   const validateEmailHandler = (e) => {
     dispatchEmailReducer({
-      type: "INPUT_BLUR", val: e.target.value,
+      type: "INPUT_BLUR",
+      val: e.target.value,
     });
   };
 
   const validatePasswordHandler = (e) => {
     dispatchPasswordReducer({
-      type: "INPUT_BLUR", val: e.target.value,
+      type: "INPUT_BLUR",
+      val: e.target.value,
     });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, pwState.value);
+    if (formIsValid) {
+      ctx.onLogin(emailState.value, pwState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.activate(); 
+    } else {
+      passwordInputRef.current.activate();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            pwState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={pwState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          ref={emailInputRef}
+          id="email"
+          label="E-mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          ref={passwordInputRef}
+          id="password"
+          label="Password"
+          type="password"
+          isValid={pwIsValid}
+          value={pwState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
